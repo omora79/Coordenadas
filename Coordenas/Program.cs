e4ac2dc5126eff9d4c7bcb5c0a86b3e77cb74ee9
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 13:42:14 -0500

    terminado

--- Coordenas/Program.cs
+++ Coordenas/Program.cs
@@ -9,6 +9,28 @@ public class Coordenadas
     {
 
         // 2 definer las variables
+        double x1, x2, y1, y2;
+        double d;
+        
+        Console.WriteLine("Ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de y1: ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de x2: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        // 3 hacer los calculos
+
+        d = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );
+
+        //4 mostara en pantalla
+
+        Console.WriteLine("la distancia entre " + x1 + " , " + y1 + " y " + x2 + " , " + y2 + " es: " + d);
 
     }
 
