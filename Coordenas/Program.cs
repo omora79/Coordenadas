78128d20fe68054ecfb883c7d0d7300f683fd10f
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 13:10:38 -0500

    definicion de variables

---
+++ Coordenas/Program.cs
@@ -0,0 +1,16 @@
+﻿// Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos.
+
+// 1 crear clase y metodo
+
+public class Coordenadas
+{
+
+    static void Main(string[] Args)
+    {
+
+        // 2 definer las variables
+
+    }
+
+
+}   
